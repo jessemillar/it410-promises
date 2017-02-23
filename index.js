1782eeb46a28d60f22e4e660dd60ccaab31844f7
Jesse Millar <hellojessemillar@gmail.com>
2017-02-22 17:32:33 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var resolvedPath = function(directoryPath, fileName) {
+	return null;
+};
+
+module.exports = resolvedPath;
+
+var readFile = function() {
+	return null;
+};
+
+module.exports = readFile;
+
+var readDir = function() {
+	return null;
+};
+
+module.exports = readDir;
+
+var readDirFiles = function() {
+	return null;
+};
+
+module.exports = readDirFiles;
