ef4d23b5f5b1fba32374e8b2b897eab577040869
Jesse Millar <hellojessemillar@gmail.com>
2017-02-22 17:46:58 -0700

    Trying to pass more tests

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports.resolvedPath = function(directoryPath, fileName) {
-	console.log(path.resolve(directoryPath, fileName));
 	return path.resolve(directoryPath, fileName);
 };
 
-module.exports.readFile = function(fileName, callback) {
+module.exports.readFile = function(fileName) {
 	fs.readFile(fileName, 'utf8', function(err, res) {
-		if (err) return callback(err);
-
 		try {
 			res = JSON.parse(res);
-		} catch (ex) {
-			return callback(ex);
 		}
-
-		callback(null, res);
 	});
 };
 
-module.exports.readDir = function() {
+module.exports.readDir = function(directoryPath) {
 	return null;
 };
 
-module.exports.readDirFiles = function() {
+module.exports.readDirFiles = function(directoryPath) {
 	return null;
 };
